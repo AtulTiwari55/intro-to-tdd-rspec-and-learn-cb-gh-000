819e22081224a7c63e7ae5c6d817f08667d54d99
Atul Tiwari <atul.tiwari@krishnacollege.ac.in>
2020-06-01 09:19:17 +0000

    Done.

---
+++ current_age_for_birth_year.rb
@@ -0,0 +1,8 @@
+def current_age_for_birth_year(birth_year)
+  current_year=2003
+  age=current_year-birth_year
+  return age
+end
+
+  age_of_person=current_age_for_birth_year(1984)
+  puts(age_of_person)
